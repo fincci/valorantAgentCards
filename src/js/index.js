4165d6f0a6d7189c29682825e86cb6d28011fc9c
filipe carvalho <filiperoc@hotmail.com>
2022-06-23 11:36:10 -0300

    list api

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,25 @@
+teste()
+
 async function teste() {
     const url = `https://valorant-api.com/v1/agents`
     const response = await fetch(url);
-    const json = await response.json();   
-    console.log(json);
+    const json = await response.json();
+    await createList(json);
+    // await createCard(json)
 }
 
-// teste();
+async function createList(json) {
+    const agents = json.data
+    for (let i = 0; i < agents.length; i++) {
+        let agent = agents[i];
+        if (agent.isPlayableCharacter) {
+            const list = document.querySelector('.agent-list')
+            const name = agent.displayName
+            const agentImg = agent.displayIcon
+            list.innerHTML +=
+                `<li class="agent" id="${name}">
+                    <img src="${agentImg}" alt="Personagem ${name}">
+                </li>`
+        }
+    }
+}
