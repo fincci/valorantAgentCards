f9e8f6a4374f8451e47aaef1d4d4f9f0dab40285
filipe carvalho <filiperoc@hotmail.com>
2022-06-23 00:52:26 -0300

    first commit

---
+++ src/js/index.js
@@ -0,0 +1,8 @@
+async function teste() {
+    const url = `https://valorant-api.com/v1/agents`
+    const response = await fetch(url);
+    const json = await response.json();   
+    console.log(json);
+}
+
+teste();
